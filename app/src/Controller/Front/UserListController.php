e02b61cb85f41f0a4b5aafa1da0623d0c43cec5d
Gabriel Fonseca <gdfonseca@leroymerlin.com.br>
2022-12-07 15:34:12 -0800

    feat(users): add node container

---
+++ app/src/Controller/Front/UserListController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller\Front;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserListController extends AbstractController
+{
+    #[Route('/')]
+    public function list(): Response
+    {
+        return $this->render('user/list.html.twig', [
+            'title' => 'Users List',
+            'description' => 'Your awesome users list.',
+            'author' => 'Gabriel Fonseca',
+        ]);
+    }
+}
